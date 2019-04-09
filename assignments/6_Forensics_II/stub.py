84be94cc3b731bce3ef1d181685b2c2c2ea28ef5
Esther Malone <esther.m.malone@gmail.com>
2019-04-09 12:29:18 -0400

    update

--- assignments/6_Forensics_II/stub.py
+++ assignments/6_Forensics_II/stub.py
@@ -43,48 +43,47 @@ timestamp, = struct.unpack("<L", data[i:i+4])
 i+=4
 author, = struct.unpack("<8s", data[i:i+8]);
 i+=8
-section_count = struct.unpack("<L", data[i:i+4]);
+section_count, = struct.unpack("<L", data[i:i+4]);
 i+=4
 
 print(" TIMESTAMP: " + str(timestamp))
-	print(" AUTHOR: %s" % author)
-	print(" SECTION_COUNT: %d" % section_count)
+print(" AUTHOR: %s" % author)
+print(" SECTION_COUNT: %d" % section_count)
 
-	print("-------  BODY  ------- ")
+print("-------  BODY  ------- ")
 
-	start = 24 
-	end = start + 8
-	extra_secionts = 2 
-	for i in  range(section_count + extra_secionts):
-		section_type, section_length = struct.unpack("<LL", data[start:end])
-		section_number = i + 1
+start = 24 
+end = start + 8
+for i in range(0, section_count + 2):
+    section_type, section_length = struct.unpack("<LL", data[start:end])
+    section_number = i + 1
 		
-		print(" SECTION NUMBER: %d" % section_number)
-		print(" SECTION_TYPE: %d" % section_type)
-		print(" SECTION_LENGTH: %d" % section_length)
+    print(" SECTION NUMBER: %d" % section_number)
+    print(" SECTION_TYPE: %d" % section_type)
+    print(" SECTION_LENGTH: %d" % section_length)
 
-		start_of_section = end
-		end_of_section = start_of_section + section_length
-		print("New Section:)
+    start_of_section = end
+    end_of_section = start_of_section + section_length
+    print("New Section")
 		
-		if section_type == 1:
-			print(" SECTION_ASCII ")
-			contents = struct.unpack(str(section_length) + "s", data[start_of_section:end_of_section])
-			print(" CONTENTS: %s\n" % contents)
-		elif section_type == 2:
-			print(" SECTION_UTF8 ")
-			contents = struct.unpack(str(section_length) + "s", data[start_of_section:end_of_section])
-			print(" CONTENTS: %s\n" % contents)
-		elif section_type == 3:
-			print(" SECTION_WORDS ")
-			contents = struct.unpack("<%dL" % (section_length/4), data[start_of_section:end_of_section])
-			print(" CONTENTS: %s\n" % unicode(contents)
+    if section_type == 1:
+	print(" SECTION_ASCII ")
+	contents = struct.unpack(str(section_length) + "s", data[start_of_section:end_of_section])
+	print(" CONTENTS: %s\n" % contents)
+    
+    elif section_type == 2:
+	print(" SECTION_UTF8 ")
+	contents = struct.unpack(str(section_length) + "s", data[start_of_section:end_of_section])
+	print(" CONTENTS: %s\n" % contents)
+    
+    elif section_type == 3:
+	print(" SECTION_WORDS ")
+	contents = struct.unpack("<%dL" % (section_length/4), data[start_of_section:end_of_section])
+	print(" CONTENTS: %s\n" % unicode(contents))
 			
-		else:
-			print("")
-			sys.exit()
-		start = end_of_section 
-		end = start + 8
+
+    start = end_of_section 
+    end = start + 8
 
 if __name__ == "__main__":
 	main()
